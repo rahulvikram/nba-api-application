28a4d72336f2f3db24393e8ba0d618c50f51ac9c
Rahul Vikram <rahulvikram479@gmail.com>
2022-01-12 22:48:49 -0800

    finished styles, added team codes

--- src/suss.js
+++ src/suss.js
@@ -1,12 +1,13 @@
 const enterButton = document.getElementById('team-btn');
 const teamData = document.getElementById('team-data');
 const rosterData = document.getElementById('roster-data')
+const clearButton = document.getElementById('clear-btn')
 
 enterButton.addEventListener('click', (e) => {
     e.preventDefault();
     const userInput = document.getElementById('input-box').value;
 
-    fetch(`https://api-nba-v1.p.rapidapi.com/teams/nickName/${userInput.replace(/\s/g,'').toLowerCase()}`, {
+    fetch(`https://api-nba-v1.p.rapidapi.com/teams/shortName/${userInput.replace(/\s/g,'').toLowerCase()}`, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
@@ -18,20 +19,38 @@ enterButton.addEventListener('click', (e) => {
         if(userInput.replace(/\s/g,'') == ''){
 
         } else {
-            let output = `
-            <div class="team">
-                <div class="image">
-                    <a href="${response.api.teams[0].logo}" target="_blank"><img class="team-img" src="${response.api.teams[0].logo}"></a>
+            if(response.api.teams[0].shortName.toLowerCase() == "uta"){
+                let output = `
+                <div class="team">
+                    <div class="image">
+                        <a href=https://www.espn.com/nba/team/_/name/utah target="_blank"><img class="team-img" src="${response.api.teams[0].logo}"></a>
+                    </div>
+                    <div class="information">
+                        <p><strong>Team: </strong>${response.api.teams[0].fullName}</p>
+                        <p><strong>Team Code: </strong>${response.api.teams[0].shortName}</p>
+                        <p><strong>Conference: </strong>${response.api.teams[0].leagues.standard.confName}</p>
+                        <p><strong>Division: </strong>${response.api.teams[0].leagues.standard.divName}</p>
+                    </div>
                 </div>
-                <div class="information">
-                    <p><strong>Team: </strong>${response.api.teams[0].fullName}</p>
-                    <p><strong>Team Code: </strong>${response.api.teams[0].shortName}</p>
-                    <p><strong>Conference: </strong>${response.api.teams[0].leagues.standard.confName}</p>
-                    <p><strong>Division: </strong>${response.api.teams[0].leagues.standard.divName}</p>
+                `
+                teamData.innerHTML += output
+            } else{
+                let output = `
+                <div class="team">
+                    <div class="image">
+                        <a href=https://www.espn.com/nba/team/_/name/${response.api.teams[0].shortName} target="_blank"><img class="team-img" src="${response.api.teams[0].logo}"></a>
+                    </div>
+                    <div class="information">
+                        <p><strong>Team: </strong>${response.api.teams[0].fullName}</p>
+                        <p><strong>Team Code: </strong>${response.api.teams[0].shortName}</p>
+                        <p><strong>Conference: </strong>${response.api.teams[0].leagues.standard.confName}</p>
+                        <p><strong>Division: </strong>${response.api.teams[0].leagues.standard.divName}</p>
+                    </div>
                 </div>
-            </div>
-            `
-            teamData.innerHTML += output
+                `
+                teamData.innerHTML += output
+            }
+
         }
 
     })
@@ -39,22 +58,8 @@ enterButton.addEventListener('click', (e) => {
         console.log("Error: Invalid entry.")
     });
 
-    // fetch("https://api-nba-v1.p.rapidapi.com/players/teamId/22", {
-    // 	"method": "GET",
-    // 	"headers": {
-    // 		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-    // 		"x-rapidapi-key": "93954fd82dmsh255f82c392d1a04p196711jsna56c443a8743"
-    // 	}
-    // })
-    // .then(response => response.json())
-    // .then(response => {
-    //     const playerList = response.api.players
-    //     console.log(playerList.length)
-    //     // for(i = 0; i < player)
-    // })
-    // .catch(err => {
-    // 	console.error(err);
-    // });
 })
 
-
+clearButton.addEventListener('click', () => {
+    teamData.innerHTML = ''
+})
